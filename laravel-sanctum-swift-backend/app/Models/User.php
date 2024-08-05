37fd783bcac71ff7629f3bbb1a2e6b60cda623e7
SK3620 <k.n.t11193620@gmail.com>
2024-08-05 15:30:27 +0900

    Add: ToDoモデルの作成, リレーションの設定

--- laravel-sanctum-swift-backend/app/Models/User.php
+++ laravel-sanctum-swift-backend/app/Models/User.php
@@ -42,4 +42,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+     // Userが持つ複数のTodoを取得するリレーション
+     public function todos()
+     {
+         return $this->hasMany(Todo::class);
+     }
 }
